bfc9f2bf4a2ef5ee14d753c332685c607f3e03e7
Jamie <bluestyluscat@gmail.com>
2025-01-20 19:09:02 -0600

    So many changes!!!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,13 @@ public class Player : MonoBehaviour
 
     public bool disableInputs;
 
+    private Vector2 lastDirection = Vector2.down; // Default facing direction (down)
+
+    // Reference to the animation system
+    public NPCAnimations anim;
+
+    public int animState = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,22 +50,36 @@ public class Player : MonoBehaviour
             //movement inputs
             float horizontalInputs = Input.GetAxisRaw("Horizontal");
             float verticalInputs = Input.GetAxisRaw("Vertical");
-            if (!disableInputs) {
+            if (!disableInputs)
+            {
                 rigidBody.velocity = new Vector2(walkSpeed * horizontalInputs, walkSpeed * verticalInputs);
             }
 
-            //Debug.Log(_playerState);
+            // Determine walking or idle state
+            Vector2 movement = rigidBody.velocity;
+            int newPlayerState = animState;
 
-            /* if completed_a_level:
-             *  Instantiate(PlayerPrefab, transform.position, Quaternion.identity);
-             *  SceneManager.LoadScene(use the scene build index);
-             *  go to file > build settings > drag new scene to the build settings
-             *  to go to next level just do like this:
-             *  int toLoad = SceneManager.GetActiveScene.buildIndex + 1;
-             *  SceneManager.LoadScene(toLoad); > goes to next scene in build settings
-             * if die:
-             *  Reload Scene somehow
-             */
+            if (movement.magnitude > 0.1f) // Player is moving
+            {
+                // Update direction based on input
+                if (horizontalInputs > 0) lastDirection = Vector2.right;
+                else if (horizontalInputs < 0) lastDirection = Vector2.left;
+                else if (verticalInputs > 0) lastDirection = Vector2.up;
+                else if (verticalInputs < 0) lastDirection = Vector2.down;
+
+                // Set walking state based on direction
+                if (lastDirection == Vector2.down) newPlayerState = -1; // Walk down
+                else if (lastDirection == Vector2.left) newPlayerState = 2;  // Walk left
+                else if (lastDirection == Vector2.up) newPlayerState = -3; // Walk up
+                else if (lastDirection == Vector2.right) newPlayerState = 4; // Walk right;
+            }
+
+            // Only update animation if the state changes
+            if (newPlayerState != animState)
+            {
+                animState = newPlayerState;
+                anim.ChangeAnimation(animState);
+            }
         }
     }
 }
